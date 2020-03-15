5c3a759e4d4e1e05cdba036fc735c730cbb6907c
Willem Hoek <willem@matimba.com>
2020-03-15 19:09:13 +0000

    Update hello.ml

--- ml/hello.ml
+++ ml/hello.ml
@@ -4,17 +4,24 @@ https://github.com/ocaml/ocaml/
 
 The stdlib module contains numbers, booleans, strings, exceptions, references, lists, arrays,
 input-output channels, ...   This module is automatically opened at the beginning of each compilation.
+All components of this module can therefore be referred by their short name.
 
-For code below - Array is in standard library, Unix is not -- so need to be added during compilation
+Therefore array is in standard library, unix is not
 
 To create a native executable:
-ocamlfind ocamlopt -linkpkg -package unix hello.ml -o hello.exe
+
+ocamlfind ocamlopt -linkpkg -package unix map1.ml -o map1.exe
 
 *)
 
 let () = 
   let d = Unix.gettimeofday() in
   let b = Array.make 10_000_000 1.0 in
-  print_float (Unix.gettimeofday() -. d);
-  print_float b.(100)
-  
+  let lx = [1;2;3;4] in
+  let ly = List.map (fun x -> x * x) lx in
+  Printf.printf  "\nb.(100) = %f\n%!"  b.(100);
+  List.iter  (Printf.printf "%d ") lx;
+  List.iter  (Printf.printf "%d ") ly;
+  Printf.printf "\n%f"(Unix.gettimeofday() -. d)
+
+
