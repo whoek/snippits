fb2e52d774f958d72394aa4611304903f1c41348
Willem <willem@matimba.com>
2020-03-15 20:33:14 +0000

    auto update from remote

--- ml/test1.ml
+++ ml/test1.ml
@@ -1,27 +1,33 @@
-(*   
-
-https://github.com/ocaml/ocaml/
+(*
 
 The stdlib module contains numbers, booleans, strings, exceptions, references, lists, arrays,
 input-output channels, ...   This module is automatically opened at the beginning of each compilation.
-All components of this module can therefore be referred by their short name.
+Example array is in standard library, unix is not.  
+To see what packages need separate linking, run 'ocamlfind list'
 
-Therefore array is in standard library, unix is not
+Use this to compile code below to native executable :
 
-To create a native executable:
+    ocamlfind ocamlopt -o map1.exe -linkpkg -package unix test1.ml
 
-ocamlfind ocamlopt -linkpkg -package unix map1.ml -o map1.exe
+or use following dune file
+
+```
+(executable
+ (name test1)
+ (libraries unix))
+```
 
 *)
 
-let () = 
+
+let () =
   let d = Unix.gettimeofday() in
   let b = Array.make 10_000_000 1.0 in
-  let lx = [1;2;3;4] in
+       let lx = [1;2;3;4] in
   let ly = List.map (fun x -> x * x) lx in
   Printf.printf  "\nb.(100) = %f\n%!"  b.(100);
   List.iter  (Printf.printf "%d ") lx;
+  print_string "\n";
   List.iter  (Printf.printf "%d ") ly;
   Printf.printf "\n%f"(Unix.gettimeofday() -. d)
-
 
