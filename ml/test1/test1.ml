d6ecfca775595937382b9411da2fb536be8b7b61
Willem Hoek <willem@matimba.com>
2020-03-22 19:48:38 +0000

    Update test1.ml

--- ml/test1/test1.ml
+++ ml/test1/test1.ml
@@ -1,22 +1,15 @@
 (*
 
-The stdlib module contains numbers, booleans, strings, exceptions, references, lists, arrays,
-input-output channels, ...   This module is automatically opened at the beginning of each compilation.
-Example array is in standard library, unix is not.  
-To see what packages need separate linking, run 'ocamlfind list'
-
 Use this to compile code below to native executable :
-
     ocamlfind ocamlopt -o map1.exe -linkpkg -package unix test1.ml
 
 or use following dune file
-
 ```
 (executable
  (name test1)
  (libraries unix))
 ```
-
+Then:
 dune build
 dune exec ./test1
 
